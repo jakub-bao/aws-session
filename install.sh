5006481f69b5e3865e76b6073330a76807b46090
Jakub <jflaska@baosystems.com>
2021-06-14 13:43:51 -0400

    working

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/bash
 cd sessionIdEncrypter
 npm i
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/bash
 echo "Running PDAP/DHIS2 Session Encrypter"
 (cd sessionIdEncrypter && node node_modules/ts-node/dist/bin.js index.ts)
