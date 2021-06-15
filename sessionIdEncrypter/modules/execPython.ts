5747e65fae8a25f93b1c20b941335cd1ed0cd10f
Jakub <jflaska@baosystems.com>
2021-06-15 15:09:29 -0400

    enc

--- sessionIdEncrypter/modules/execPython.ts
+++ sessionIdEncrypter/modules/execPython.ts
@@ -4,7 +4,7 @@ export async function execPython(params:string[]):Promise<string>{
     return new Promise((resolve,reject)=>{
         let python = spawn('python3',params)
         python.stdout.on('data',(data:Buffer)=>{
-            let asString = data.toString();
+            let asString = data.toString().replace(/\n/,'');
             resolve(asString);
         })
         python.stderr.on('data',(data)=>{
