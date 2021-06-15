7b593945fb529012bd5028fb97523b03be78f689
Jakub <jflaska@baosystems.com>
2021-06-15 15:08:49 -0400

    enc

--- sessionIdEncrypter/modules/encrypt.ts
+++ sessionIdEncrypter/modules/encrypt.ts
@@ -1,5 +1,5 @@
 import {execPython} from "./execPython";
 
 export function encrypt(key:string, message:string):Promise<string>{
-    return execPython(['../encrypt/fernet',key, message])
+    return execPython(['../encrypt/fernetEncrypt.py',key, message])
 }
